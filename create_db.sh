b53b2823f51e6c2e1cc62818a6ac9d5555df3aeb
troysadkowsky <troy.sadkowsky@gmail.com>
2014-11-07 21:17:41 +1000

    eclipse project files added

--- create_db.sh
+++ create_db.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -x
 set -e
-mysql -u root  << EOF
+mysql -u root -ppassword << EOF
 drop database if exists TimeGone;
 create database TimeGone default character set "utf8";
 grant all on TimeGone.* to TimeGone@localhost identified by "TimeGone";
 EOF
-cd TimeGone
+
 ./manage.py syncdb --noinput
 ./manage.py loaddata initial_data
 echo "You can 'cd TimeGone && ./manage.py runserver' now"
