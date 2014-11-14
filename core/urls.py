81b72f4470e5d053ea98e6dbe53b41ec9152384e
Alexander Pugachev <alexander.pugachev@gmail.com>
2014-11-14 10:08:52 +0200

    1

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,5 @@ from core import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
+    url(r'^/auth/register/$', views.register, name='register') 
 )
--- core/views.py
+++ core/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import render
+from django.auth.decorators import login_required
+from django.shortcuts import render, redirect
+
+from forms import RegiserForm
 from models import Project
 
 
+@login_required
 def index(request):
     return render(request, 'index.html', {'objects': Project.objects.all()})
 
 
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST.data)
+        if form.is_valid():
+            user = User.objects.create(username=form.cleaned_data['username'])
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            # login user
+            # redirect
+    else:
+        form = RegisterForm()
+    return render(request, 'register.html', {'form': form})
