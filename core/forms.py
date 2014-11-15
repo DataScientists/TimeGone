abf61a9f49d497e3f8ced655eceb80a68bd86618
Alexander Pugachev <alexander.pugachev@gmail.com>
2014-11-15 12:53:52 +0200

    User login added (task-183)

--- core/forms.py
+++ core/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from models import Project
 
+
 class RegisterForm(forms.Form):
     username = forms.CharField()
     email = forms.EmailField()
@@ -14,7 +15,12 @@ class RegisterForm(forms.Form):
         if (p and c and p != c):
             self.add_error('confirm', 'Does not match')
 
-            
+
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+
+
 class AddForm(forms.ModelForm):
     class Meta:
         model = Project
--- core/views.py
+++ core/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.views.decorators.csrf import csrf_protect
 
-from forms import RegisterForm, AddForm
+from forms import RegisterForm, AddForm, LoginForm
 from models import Project
 from django.db.utils import IntegrityError
 
@@ -13,6 +13,7 @@ from django.db.utils import IntegrityError
 def index(request):
     qs = Project.objects.filter(user=request.user)
     return render(request, 'index.html', {'objects': qs})
+
 
 @login_required
 def add(request):
@@ -27,28 +28,53 @@ def add(request):
         f = AddForm()
     return render(request, 'add.html', {'form': f})
 
+
 @csrf_protect
 def register(request):
     if request.method == 'POST':
-        form = RegisterForm(request.POST)
-        if form.is_valid():
-            try:
-                User.objects.create_user(
-                    form.cleaned_data['username'],
-                    form.cleaned_data['email'],
-                    form.cleaned_data['password'])
-            except IntegrityError, e:
-                if 'username' in str(e):
-                    form.add_error('username', 'Username is taken')
-                elif 'email' in str(e):
-                    form.add_error('email', 'Email is already registered')
+        a = request.POST.get('action')
+        if a == 'register':
+            register_f = RegisterForm(request.POST)
+            login_f = LoginForm()
+            if register_f.is_valid():
+                try:
+                    User.objects.create_user(
+                        register_f.cleaned_data['username'],
+                        register_f.cleaned_data['email'],
+                        register_f.cleaned_data['password'])
+                except IntegrityError, e:
+                    if 'username' in str(e):
+                        register_f.add_error('username',
+                                             'Username is taken')
+                    elif 'email' in str(e):
+                        register_f.add_error('email',
+                                             'Email is already registered')
+                    else:
+                        raise e
                 else:
-                    raise e
-            else:
-                user = authenticate(username=form.cleaned_data['username'],
-                                    password=form.cleaned_data['password'])
-                login(request, user)
-                return redirect('index')
+                    user = authenticate(
+                        username=register_f.cleaned_data['username'],
+                        password=register_f.cleaned_data['password'])
+                    login(request, user)
+                    return redirect('index')
+        elif a == 'login':
+            register_f = RegisterForm()
+            login_f = LoginForm(request.POST)
+            if login_f.is_valid():
+                user = authenticate(
+                    username=login_f.cleaned_data['username'],
+                    password=login_f.cleaned_data['password'])
+                if user is None:
+                    login_f.add_error('username',
+                                      'Wrong credentials')
+                else:
+                    login(request, user)
+                    return redirect('index')
+        else:
+            register_f = RegisterForm()
+            login_f = LoginForm()
     else:
-        form = RegisterForm()
-    return render(request, 'register.html', {'form': form})
+        register_f = RegisterForm()
+        login_f = LoginForm()
+    return render(request, 'register.html', {'register_form': register_f,
+                                             'login_form': login_f})
